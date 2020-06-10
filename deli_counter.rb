56ede52968094befec0eb4567c541bd62eb1cdc3
resourceful-composition-1273 <cozycosmic@gmail.com>
2020-06-10 16:26:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,62 @@ end
 
 #method name    #argument
 def line(katz_deli)
-if line()
-else "The line is currently empty."
-puts katz_deli.length(name) #=>
+  if katz_deli == []
+  puts "The line is currently empty."
+  else 
+  result = "The line is currently:"
+  katz_deli.each_with_index{ |name, index |result=result+" #{index+1}. #{name}" }
+  puts result
+  end
 end
+
+#? if condition to check if line is empty
+        #? puts statement for empty line
+        #? else
+        #? code to output line with all names and numbers
+        #? end
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#line "the line is currently empty."
+
+
+
+
+
+#method name    #argument
+def now_serving(katz_deli) #=>
+  if katz_deli.length>0 #=>
+  puts "Currently serving #{katz_deli.shift}." 
+  else 
+  puts "There is nobody waiting to be served!"
+  end
+end        
+        
+        
+        
